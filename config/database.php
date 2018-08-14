d77de7d848ae909355023d45b9237258cd5acb52
b3n10 <francisben.lleve@gmail.com>
2018-08-14 20:26:41 +0800

    Configure to use heroku env var

--- config/database.php
+++ config/database.php
@@ -1,5 +1,21 @@
 <?php
 
+$host = env('DB_HOST');
+$port = env('DB_PORT');
+$database = env('DB_DATABASE');
+$username = env('DB_USERNAME');
+$password = env('DB_PASSWORD');
+
+if ($db_url = getenv('DATABASE_URL')) {
+	$url = parse_url($db_url);
+
+	$host = $url['host'];
+	$port = $url['port'];
+	$database = ltrim($url['path'], '/');
+	$username = $url['user'];
+	$password = $url['pass'];
+}
+
 return [
 
     /*
@@ -56,11 +72,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'port' => $port,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
