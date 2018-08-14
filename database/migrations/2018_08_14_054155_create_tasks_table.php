5b07026291b9e0731dd519dbde7e3586485278bb
b3n10 <francisben.lleve@gmail.com>
2018-08-14 21:01:31 +0800

    Edit to test migration

--- database/migrations/2018_08_14_054155_create_tasks_table.php
+++ database/migrations/2018_08_14_054155_create_tasks_table.php
@@ -14,9 +14,11 @@ class CreateTasksTable extends Migration
 	public function up()
 	{
 		Schema::create('tasks', function (Blueprint $table) {
+
 			$table->increments('id');
 			$table->string('name');
 			$table->timestamps();
+
 		});
 	}
 
